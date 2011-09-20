d69d607ccce5c69be1d0e22ba35b207dd5c9f2e2
Adrian Aisemberg <adrianaisemberg@gmail.com>
2011-09-20 13:38:23 +0200

    Missing ErrorAttribute.cs file

---
+++ CLAP/ErrorAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CLAP
+{
+    [Serializable]
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class ErrorAttribute : Attribute
+    {
+        public bool ReThrow { get; set; }
+    }
+}
