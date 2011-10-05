a937e234ac95fc0f77854d71ba5bed894d3e4664
Adrian Aisemberg <adrianaisemberg@gmail.com>
2011-10-05 23:14:06 +0200

    Added some more web content

--- Web/script.js
+++ Web/script.js
@@ -8,15 +8,23 @@ $(function(){
         
         // expand sub
         var visible_sub_menu = $(".sub_menu:visible");
-        if (visible_sub_menu.attr("menu_id") != href)
+        if (visible_sub_menu.length > 0 && visible_sub_menu.attr("menu_id") != href)
         {
-            visible_sub_menu.fadeOut(200);
+            visible_sub_menu.fadeOut(200, function(){
+                fadeSubMenuIn(href);
+            });
         }
-        
-        $(".sub_menu[menu_id='" + href + "']").fadeIn(400);
+        else
+        {
+            fadeSubMenuIn(href);
+        }
         
         return false;
     });
     
     $("#home").show();
 });
+
+function fadeSubMenuIn(href){
+    $(".sub_menu[menu_id='" + href + "']").fadeIn(400);
+}
