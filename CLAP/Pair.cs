a8854bf2a808fda0dcd49a65003ba149314a91bb
Adrian Aisemberg <adrianaisemberg@gmail.com>
2012-04-05 21:33:51 +0200

    pair

---
+++ CLAP/Pair.cs
@@ -0,0 +1,23 @@
+﻿
+namespace CLAP
+{
+    internal class Pair<TFirst, TSecond>
+    {
+        public TFirst First { get; private set; }
+        public TSecond Second { get; private set; }
+
+        public Pair(TFirst first, TSecond second)
+        {
+            First = first;
+            Second = second;
+        }
+    }
+
+    internal static class Pair
+    {
+        internal static Pair<TFirst, TSecond> Create<TFirst, TSecond>(TFirst first, TSecond second)
+        {
+            return new Pair<TFirst, TSecond>(first, second);
+        }
+    }
+}
