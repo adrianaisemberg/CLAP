e842c3d37a80c64a518ada27275c315f63130a4e
Hugh <hugh_gleaves@hotmail.com>
2017-09-23 14:39:26 -0700

    Added second simple project to see what AppVeyor does.

---
+++ CrudeApp/CrudeApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
